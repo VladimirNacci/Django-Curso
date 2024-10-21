a9c96f3169599e19243b3ba6eea7c4d8bbb2ae62
Vladimir Nacci <vlad.br@gmail.com>
2024-10-21 11:35:45 -0300

    Removido Contato e Sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
-def home_view(request):
+def home(request):
     return render(request, 'recipes/home.html')
-
-
-def contato_view(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre_view(request):
-    return render(request, 'recipes/sobre.html')
